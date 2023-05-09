7697d70f82e9652b4d0e205570513e358ebe95df
itsneedtoknow <letta_in_love@mail.ru>
2023-05-09 14:19:30 +0300

    feat: add solution to task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+
+
+
+fs.readdir(path.join(__dirname, 'files'), (err, files) =>{
+    if(err) throw err;
+
+    fs.access(path.join(__dirname, 'filescopy'), err =>{
+
+        if (err && err.code === 'ENOENT') {
+            fs.mkdir(path.join(__dirname, 'filescopy'), err=>{
+                if (err) throw err;
+            });
+
+            files.forEach(file =>{
+            fs.stat(path.join(__dirname, 'files', file), (err, stat) =>{
+            if(err) {throw err;}
+            if (stat.isFile()){
+            fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'filescopy', file), err =>{
+            if(err) {throw err;}
+            });
+        
+             }
+            })
+
+        })
+        }
+        
+    })
+    
+
+})
 
